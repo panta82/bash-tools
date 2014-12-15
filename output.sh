f7f32b725d3b79e3526b475d2ef0c491cbb80bdd
Ivan Pantic <ivanpantic82@gmail.com>
2014-12-15 15:15:29 +0100

    Added colors, badge centering

--- output.sh
+++ output.sh
@@ -4,26 +4,55 @@
 
 # Global settings
 
-_DEFAULT_ERROR_SIGNAL_=1
+	# If nothing is supplied, this is the signal process will error out with
+	_DEFAULT_ERROR_SIGNAL_=1
 
-# Output message to stdout. Optional suffix as second arg
+	# Verbosity level, influences the way all output functions are acting
+	_VERBOSITY_LEVEL_=3
+
+	# Equalize all badges to this width. Clear to disable
+	_BADGE_WIDTH_=7
+
+	# If true, output is color-coded
+	_COLORS_=true
+
+# Output message to stdout.
+#     msg: Message to print
+#     badge: Optional badge to add before msg
+#     badge_color: Style to use for badge, if COLORS is enabled. Otherwise, used [BADGE] style.
+#     msg_color: Style to use for msg, if COLORS is enabled
+# Colors should be strings compiled from standard escape codes, see here: http://misc.flogisoft.com/bash/tip_colors_and_formatting
 _stdout() {
 	local msg="$1"
-	local prefix="$2"
-	if [ -z "$prefix" ]; then
-		echo "$msg"
-	else
-		if [ ! -z "$msg" ]; then
-			echo "[$prefix] $msg"
+	local badge="$2"
+	local badge_color="$3"
+	local msg_color="$4"
+	
+	[[ -z "$msg" ]] && echo '' && return
+
+	$_COLORS_ && [[ ! -z "$msg_color" ]] && msg="${msg_color}$msg\\e[0m"
+	
+	if [[ ! -z $badge ]]; then
+		if [[ ! -z $_BADGE_WIDTH_ ]]; then
+			local before_badge
+			local after_badge
+			(( before_badge = ( ${_BADGE_WIDTH_} - ${#badge} ) / 2 ))
+			(( after_badge = ${_BADGE_WIDTH_} - $before_badge - ${#badge} ))
+			badge=$( printf "%${before_badge}s%s%${after_badge}s" '' $badge '' )
+		fi
+		if $_COLORS_ ; then
+			[[ ! -z "$badge_color" ]] && badge="${badge_color}$badge\\e[0m"
 		else
-			echo ""
+			badge="[$badge]"
 		fi
 	fi
+
+	echo -e "${badge} ${msg}"
 }
 
 # Output to stderr
 _stderr() {
-	_stdout "$1" "$2" 1>&2
+	_stdout "$@" 1>&2
 }
 
 # Exit with _DEFAULT_ERROR_SIGNAL_ or provided signal
@@ -33,20 +62,30 @@ _exit_with_error() {
 	exit $exit_signal
 }
 
-# Print INFO message
+# Print INFO message, verbosity 3+
 log() {
-	_stdout "$1" "INFO"
+	[[ $_VERBOSITY_LEVEL_ -lt 3 ]] && return
+	_stdout "$1" "INFO" '\e[42m'
 }
 
-# Print WARNING message
+# Print WARNING message, verbosity 2+
 warn() {
-	_stderr "$1" "WARNING"
+	[[ $_VERBOSITY_LEVEL_ -lt 2 ]] && return
+	_stderr "$1" "WARNING" '\e[103m\e[90m'
 }
 
-# Print fatal error and exit with error code
+# Print ERROR message, verbosity 1+
+error() {
+	[[ $_VERBOSITY_LEVEL_ -lt 1 ]] && return
+	_stderr "$1" "ERROR" '\e[41m' '\e[31m'
+}
+
+# Print fatal error and exit with error code, verbosity 1+
 fatal() {
-	_stderr ""
-	_stderr "$1" "FATAL"
+	if [[ $_VERBOSITY_LEVEL_ -ge 1 ]]; then
+		_stderr ""
+		_stderr "$1" "FATAL" '\e[41m' '\e[31m'
+	fi
 	_exit_with_error "$2"
 }
 
--- test/all.sh
+++ test/all.sh
@@ -11,3 +11,28 @@ define_option "switch1" "-s1:" "Description of switch 1"
 opts_standard_sequence "$@"
 
 log "Value of switch1: ${OPT_VALUES[switch1]}"
+
+warn "Test of a warning!"
+error "Test of an error!"
+
+_VERBOSITY_LEVEL_=2
+
+warn "Lowered verbosity, you should no longer see infos!"
+log "You shouldn't see this"
+
+_VERBOSITY_LEVEL_=1
+
+error "You should see only errors now"
+warn "You shouldn't see this"
+log "You shouldn't see this"
+
+_VERBOSITY_LEVEL_=0
+
+error "You shouldn't see this"
+warn "You shouldn't see this"
+log "You shouldn't see this"
+
+_VERBOSITY_LEVEL_=5
+
+log "Testing fatal..."
+fatal "Nothing bad happened, just a test"
